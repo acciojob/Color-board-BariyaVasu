4a94b10d78cb0359f4033a44873d724ab7d62f4b
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-17 13:40:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+  square.addEventListener('mouseenter', () => {
+    square.style.backgroundColor = '#652BCD';
+  });
+
+  square.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+      square.style.backgroundColor = '#2B2B2B';
+    }, 1000); // Revert after 1 second
+  });
+});
